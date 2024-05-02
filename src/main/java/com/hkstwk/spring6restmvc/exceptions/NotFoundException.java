6c22e3a2100f46cb24ba9f6600b9e3bd573748e0
Harm Kolvoort <hkolvoort@aegon.nl>
2024-05-02 10:23:03 +0200

    87 - Exception using ResponseStatus Annotation

--- src/main/java/com/hkstwk/spring6restmvc/exceptions/NotFoundException.java
+++ src/main/java/com/hkstwk/spring6restmvc/exceptions/NotFoundException.java
@@ -1,5 +1,9 @@
 package com.hkstwk.spring6restmvc.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Value not found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException() {
     }
