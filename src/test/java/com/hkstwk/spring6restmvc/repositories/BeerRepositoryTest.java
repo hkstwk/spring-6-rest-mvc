e27159d7b25b613c5478323bec2c1749bc64270b
Harm Kolvoort <hkolvoort@aegon.nl>
2024-05-05 11:17:32 +0200

    96 - Sping Boot JPA Test Splice

--- src/test/java/com/hkstwk/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/hkstwk/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,4 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.hkstwk.spring6restmvc.repositories;
+
+import com.hkstwk.spring6restmvc.entities.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 class BeerRepositoryTest {
-  
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void saveBeer(){
+        Beer savedBeer = beerRepository.save(Beer.builder()
+                        .beerName("My new beer")
+                .build());
+
+        assertThat(savedBeer).isNotNull();
+        assertThat(savedBeer.getBeerName()).isEqualTo("My new beer");
+        assertThat(savedBeer.getId()).isNotNull();
+    }
+
 }
--- src/test/java/com/hkstwk/spring6restmvc/repositories/CustomerRepositoryTest.java
+++ src/test/java/com/hkstwk/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -1,4 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.hkstwk.spring6restmvc.repositories;
+
+import com.hkstwk.spring6restmvc.entities.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 class CustomerRepositoryTest {
-  
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void saveCustomer(){
+        Customer savedCustomer = customerRepository.save(Customer.builder()
+                        .customerName("My new customer")
+                .build());
+
+        assertThat(savedCustomer).isNotNull();
+        assertThat(savedCustomer.getCustomerName()).isEqualTo("My new customer");
+        assertThat(savedCustomer.getId()).isNotNull();
+    }
+
 }
