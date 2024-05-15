6d559f36a9fe3b74bd22b81469a8c9da6bd8e172
Harm Kolvoort <hkolvoort@aegon.nl>
2024-05-15 22:55:27 +0200

    134 - Add Database Column

--- src/main/java/com/hkstwk/spring6restmvc/entities/Customer.java
+++ src/main/java/com/hkstwk/spring6restmvc/entities/Customer.java
@@ -33,6 +33,9 @@ public class Customer {
     private UUID id;
     private String customerName;
 
+    @Column(length = 255)
+    private String email;
+
     @Version
     private Integer version;
 
