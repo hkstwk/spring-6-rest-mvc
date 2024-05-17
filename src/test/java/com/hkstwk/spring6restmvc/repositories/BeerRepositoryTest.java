b13ed8bb442e40be3d41c01e121cc732747945f3
Harm Kolvoort <hkolvoort@aegon.nl>
2024-05-17 16:24:43 +0200

    151 - Find By Name with Spring Data JPA

--- src/test/java/com/hkstwk/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/hkstwk/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,18 +1,23 @@
 package com.hkstwk.spring6restmvc.repositories;
 
+import com.hkstwk.spring6restmvc.bootstrap.BootStrapData;
 import com.hkstwk.spring6restmvc.entities.Beer;
 import com.hkstwk.spring6restmvc.model.BeerStyle;
+import com.hkstwk.spring6restmvc.services.BeerCsvServiceImpl;
 import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
+@Import({BootStrapData.class, BeerCsvServiceImpl.class})
 class BeerRepositoryTest {
 
     @Autowired
@@ -47,7 +52,12 @@ class BeerRepositoryTest {
 
             beerRepository.flush();
         });
-
     }
 
+    @Test
+    void listGetBeerListByName() {
+        List<Beer> beerList = beerRepository.findAllByBeerNameIsLikeIgnoreCase("%IPA%");
+
+        assertThat(beerList).hasSize(336);
+    }
 }
